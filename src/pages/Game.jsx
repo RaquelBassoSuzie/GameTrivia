e94379deb63fc8acd85300b0ff06835ca2cd3d0c
tgandr <tgandr@gmail.com>
2022-05-25 17:23:22 -0300

    começo do requisito 6

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import Login from './Login';
 
 class Game extends React.Component {
   render() {
+    const checkToken = localStorage.getItem('token')
+    if (checkToken === undefined) {
+      localStorage.setItem('token', '');
+      return <Login />;
+    }
     return (
       <section>
         <Header />
