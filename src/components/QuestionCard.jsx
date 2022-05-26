aded8f17c0180deca7ddb84d98279e64dccc30cd
Will-Devs <allyson-bedin@hotmail.com>
2022-05-26 15:58:22 -0300

    Requisitos 10 e 11

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,34 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { updateScoreAndAssertions } from '../redux/actions';
 
 class QuestionCard extends React.Component {
-  handleClickRight = () => {
-    const { indexQuestions, questions, countTime, updateScore, afterAnswer } = this.props;
-    const { difficulty } = questions[indexQuestions];
-    afterAnswer();
-    const difficultyScore = { hard: 3, medium: 2, easy: 1 };
-    const TEN = 10;
-    const score = TEN + (countTime * difficultyScore[difficulty]);
-    updateScore(score);
-  };
+  constructor() {
+    super();
+    this.state = {
+      showNextBtn: false,
+      indexQuestions: 0,
+    };
+  }
 
-  handleClickWrong = () => {
-    const { afterAnswer } = this.props;
-    afterAnswer();
-  };
+  answerQuestion = () => {
+    this.setState({ showNextBtn: true });
+  }
+
+  nextQuestion = () => {
+    const maxQuestions = 4;
+    const { indexQuestions } = this.state;
+    this.setState((prevState) => ({
+      showNextBtn: false,
+      indexQuestions: prevState.indexQuestions + 1,
+    }));
+    const { backToFeedback } = this.props;
+    if (indexQuestions === maxQuestions) {
+      backToFeedback();
+    }
+  }
 
   render() {
-    const { indexQuestions,
-      questions: atualQuestion,
-      countTime, isDisabled,
-    } = this.props;
-    const { question, category, answers } = atualQuestion[indexQuestions];
-
+    const { questions: atualQuestion } = this.props;
+    const { indexQuestions, showNextBtn } = this.state;
+    const { question,
+      correct_answer: correctAnswer,
+      incorrect_answers: incorrectAnswers,
+      category,
+    } = atualQuestion[indexQuestions];
+    const incorrectAnswersMap = incorrectAnswers
+      .map((answer, index) => ({ type: false, answer, index }));
+    const answers = [
+      { type: true, answer: correctAnswer, index: null },
+      ...incorrectAnswersMap,
+    ];
+    // Embaralhamentos dos elementos de um array proveniente da mentoria técnica com o instrutor Moisés Santana
+    // link da referência utilizada: https://flaviocopes.com/how-to-shuffle-array-javascript/
+    // Como o Math.random gera um número aleatório entre 0 e 0.99, reduzir 0.5 faz ele ser maior ou menor aleatoriamente resultando com comportamento do sort de ordenar em relação ao número retornado (ants ou depois)
+    const HALF_NUMBER = 0.5;
+    answers.sort(() => Math.random() - HALF_NUMBER);
     return (
       <section>
-        <p>{countTime}</p>
         <h3 data-testid="question-text">{ question }</h3>
         <p data-testid="question-category">{ category }</p>
         <div data-testid="answer-options">
@@ -39,8 +59,7 @@ class QuestionCard extends React.Component {
                   type="button"
                   key="correct-answer"
                   data-testid="correct-answer"
-                  disabled={ isDisabled }
-                  onClick={ this.handleClickRight }
+                  onClick={ this.answerQuestion }
                 >
                   { answer }
                 </button>
@@ -51,14 +70,23 @@ class QuestionCard extends React.Component {
                 type="button"
                 key={ `wrong-answer-${index}` }
                 data-testid={ `wrong-answer-${index}` }
-                disabled={ isDisabled }
-                onClick={ this.handleClickWrong }
+                onClick={ this.answerQuestion }
               >
                 { answer }
               </button>
             );
           })}
         </div>
+        { showNextBtn
+          ? (
+            <button
+              data-testid="btn-next"
+              id="btn-next"
+              type="button"
+              onClick={ this.nextQuestion }
+            >
+              Next
+            </button>) : ''}
       </section>
     );
   }
@@ -66,18 +94,10 @@ class QuestionCard extends React.Component {
 
 QuestionCard.propTypes = {
   question: PropTypes.objectOf(PropTypes.string),
-  updateScore: PropTypes.func,
-  afterAnswer: PropTypes.func,
-  indexQuestions: PropTypes.number,
-  countTime: PropTypes.number,
 }.isRequired;
 
 const mapStateToProps = (state) => ({
   questions: state.game.questions,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  updateScore: (payload) => dispatch(updateScoreAndAssertions(payload)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(QuestionCard);
+export default connect(mapStateToProps)(QuestionCard);
