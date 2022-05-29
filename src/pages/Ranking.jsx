0625e5113244c4f33a2b074cf4834aa998ba8fdb
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-05-29 13:07:29 -0300

    Requisito 16 OK - adicionar os requisitos complementares

---
+++ src/pages/Ranking.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import propTypes from 'prop-types';
+
+class Ranking extends Component {
+  render() {
+    return (
+      <section>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        { /* 18 - Gui - adicionar o botão para a tela de login */ }
+        { /* 19 - Allyson - adicionar o conteudo da página de ranking */ }
+        { /* 20 - a definir -
+        realizar os testes da tela de ranking + teste adicionar na tela de feedback */ }
+      </section>
+    );
+  }
+}
+
+// Ranking.propTypes = {
+// };
+
+export default Ranking;
